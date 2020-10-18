e1a595a175cd4bf626ec9abc38f5ef100bf79d80
Shamil <shamil.badalloffsh@gmail.com>
2020-10-18 11:47:35 +0200

    Add: state of item

--- src/App.js
+++ src/App.js
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useReducer } from "react";
 
 function App() {
   return (
     <div className="flex flex-col h-full items-center justify-center bg-gray-200 text-gray-700">
-      <div className="flex items-center">
-        <h1 className="text-6xl font-thin tracking-wider">Create React App + Tailwind CSS</h1>
-      </div>
-      <p className="my-6 tracking-wide">
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-      <div className="mt-6 flex justify-center">
-        <a
-          className="uppercase hover:underline"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a
-          className="ml-10 uppercase hover:underline"
-          href="https://tailwindcss.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Tailwind
-        </a>
-      </div>
+      Todo application
+      <TodoList />
     </div>
   );
 }
 
 export default App;
+
+const TodoReducer = function (state, action) {
+  switch (action.type) {
+    case "TOGGLE_TODO":
+      return state.map((todo, idx) =>
+        idx === action.idx ? { ...todo, completed: !todo.completed } : todo
+      );
+    default:
+      return state;
+  }
+};
+
+const TodoList = () => {
+  const [todos, dispatch] = useReducer(TodoReducer, [
+    { text: "First Task", completed: false },
+  ]);
+
+  return (
+    <div>
+      {todos.map((todo, idx) => (
+        <label key={idx} className="block select-none">
+          <input
+            onChange={() => dispatch({ type: "TOGGLE_TODO", idx })}
+            type="checkbox"
+            checked={todo.completed}
+          />
+          {todo.text}
+        </label>
+      ))}
+    </div>
+  );
+};
